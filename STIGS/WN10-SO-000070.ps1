e4e950752f7c1d5dd29cf8db92b2c85945579e89
nickpamatian <nickpamatian1337@gmail.com>
2025-04-14 22:34:00 -0500

    WN10-SO-000070.ps1

---
+++ STIGS/WN10-SO-000070.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Nick Pamatian
+    LinkedIn        : https://www.linkedin.com/in/nick-pamatian-b8828b28a/
+    GitHub          : https://github.com/nickpamatian
+    Date Created    : 2025-04-14
+    Last Modified   : 2025-04-14
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000070
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-SO-000070.ps1 
+#>
+
+# Define registry path and parameters
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$valueName = "InactivityTimeoutSecs"
+$valueData = 900  # Decimal value (0x384 in hex)
+
+# Ensure the registry key exists
+New-Item -Path $regPath -Force | Out-Null
+
+# Set the DWORD value
+Set-ItemProperty -Path $regPath -Name $valueName -Value $valueData -Type DWord
