f0a372f42bbf4b4dbf49bd9afbc851ef5a83fb4b
nickpamatian <nickpamatian1337@gmail.com>
2025-04-21 17:35:38 -0500

    WN10-SO-000070.ps1

--- STIGS/WN10-SO-000070.ps1
+++ STIGS/WN10-SO-000070.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+    This PowerShell script ensures thatthe  machine inactivity limit is set to 15 minutes, locking the system with the screensaver.
 
 .NOTES
     Author          : Nick Pamatian
